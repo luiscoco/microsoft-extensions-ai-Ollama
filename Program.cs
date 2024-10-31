636ad2d54ad2f67f2fd7ba9f325dad6ad1ae0b40
Luis Coco Enríquez <LEnriquez@luxoft.com>
2024-10-31 10:54:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.AI;
+
+IChatClient client = new OllamaChatClient(new Uri("http://localhost:11434"), "phi3:latest");
+
+var response = await client.CompleteAsync("What is AI?");
+
+Console.WriteLine(response);
